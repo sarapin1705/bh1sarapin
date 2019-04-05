3fa4f8318637c574e38a2fdc170706c2a983907a
sarapin1705 <alka418.as@mail.ru>
2019-04-05 21:32:02 +0300

    sara

--- src/by/belhard/j2/lesson2/main.java
+++ src/by/belhard/j2/lesson2/main.java
@@ -1,9 +1,26 @@
 package by.belhard.j2.lesson2;
 
 public class main {
-    int x;
+
 
     public static void main(String[] args) {
 
+        int x = 23;
+
+        if (x == 20) {
+            x--;
+        } else if (x == 21) {
+            x++;
+         else {
+                x /=2;
+            }
+        }
+
+        int y = (x % 2== 0) ? (x / 2) : -x;
+
+
+        System.out.println(x);
+
+        System.out.println(y);
     }
 }
